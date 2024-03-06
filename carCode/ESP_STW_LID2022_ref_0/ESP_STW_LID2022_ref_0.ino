51907b3dc8e39da8691ad9c8c0439c8ffefd0385
Tc_phoenix_ <tibo.coppens@outlook.com>
2024-03-06 16:35:55 +0200

    2-way send esp code additions

--- carCode/ESP_STW_LID2022_ref_0/ESP_STW_LID2022_ref_0.ino
+++ carCode/ESP_STW_LID2022_ref_0/ESP_STW_LID2022_ref_0.ino
@@ -1,8 +1,8 @@
 /*
 
-ESP Steering wheel example ref 1.2.2022 KN
-Lidar reading
-  
+  ESP Steering wheel example ref 1.2.2022 KN
+  Lidar reading
+
 */
 
 // Import required libraries
@@ -14,160 +14,189 @@ Lidar reading
 
 
 
-            //            network credentials
+//            network credentials
 
 
-//const char* ssid = "SOURCE";
-//const char* password = "Pelle!23";
+const char* ssid = "SOURCE";
+const char* password = "Pelle!23";
 
-const char* ssid = "mokkula_925936";
-const char* password = "12345678";
+//const char* ssid = "mokkula_925936";
+//const char* password = "12345678";
 
 
 bool    spiffsActive = false;
 
-                                                                        // Create AsyncWebServer object on port 80
+// Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
-String from_mega="";                                                    // lidar distance value in String format
+String from_mega = "";                                                  // lidar distance value in String format
 
 int is;
 
-char buf[40];                                                           //  Serial bus buffer   
+char buf[40];                                                           //  Serial bus buffer
 
 
- 
+
 void setup()
 {
-                                                                          // Serial port for transfer data with Mega
+  // Serial port for transfer data with Mega
   Serial.begin(9600);
-  
+
   Serial.println("");
 
   Serial.println("----------------");
 
   Serial.println("----------------");
 
-                                                                          // Start file subsystem
+  // Start file subsystem
   if (SPIFFS.begin()) {
-      Serial.println("SPIFFS Active");
-      Serial.println();
-      spiffsActive = true;
+    Serial.println("SPIFFS Active");
+    delay(100);
+    Serial.print(SPIFFS.exists("/index.html"));
+    Serial.print(",");
+    Serial.print(SPIFFS.exists("/sbar.html"));
+    Serial.print(",");
+    Serial.print(SPIFFS.exists("/jquery-1.11.3.min.js"));
+    Serial.print(",");
+    Serial.print(SPIFFS.exists("/roundslider.min.js"));
+    Serial.print(",");
+    Serial.print(SPIFFS.exists("/roundslider.min.css"));
+    Serial.print(",");
+    Serial.print(SPIFFS.exists("/compass.png"));
+    Serial.println();
+    spiffsActive = true;
   } else {
-      Serial.println("Unable to activate SPIFFS");
+    Serial.println("Unable to activate SPIFFS");
   }
 
 
-                                                                            // Connect to Wi-Fi
+  // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
 
-                                                                            // Print ESP32 Local IP Address
+  // Print ESP32 Local IP Address
   Serial.println(WiFi.localIP());
-  
-
-                                                                            // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
-                request->send(SPIFFS, "/index.html", String(), false, processor);
-
- //             Serial.println(" HTML GET ");
-
-                int paramsNr = request->params();
-
- //             Serial.println(" ------ ");
- //             Serial.print("Param qty: ");
- //             Serial.println(paramsNr);
-      
-                for (byte i=0;i<paramsNr;i++)
-                    {
-
-                        AsyncWebParameter* p = request->getParam(0);
- 
-//                      Serial.print("Param name: ");
-//                      Serial.println(p->name());
-
-                        if ((p->name())=="dist"){ Serial.print("Move:");Serial.println(p->value()); };
-                        if ((p->name())=="dire"){ Serial.print("Turn:");Serial.println(p->value()); };
-      
-   //                   Serial.print("Param value: ");
-   //                   Serial.println(p->value());
-                    };                                                          //    end of for loop
-                    
-   
-              });                                                              // server on END
 
 
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request)
+  {
+    request->send(SPIFFS, "/index.html", String(), false, processor);
 
-                                                                              // Route to load style.css file
-  server.on("/jquery-1.11.3.min.js", HTTP_GET, [](AsyncWebServerRequest *request)
+    //             Serial.println(" HTML GET ");
+
+    int paramsNr = request->params();
+
+    //             Serial.println(" ------ ");
+    //             Serial.print("Param qty: ");
+    //             Serial.println(paramsNr);
+
+    for (byte i = 0; i < paramsNr; i++)
+    {
+
+      AsyncWebParameter* p = request->getParam(0);
+
+      //                      Serial.print("Param name: ");
+      //                      Serial.println(p->name());
+
+      if ((p->name()) == "dist") {
+        Serial.print("Move:");
+        Serial.println(p->value());
+      };
+      if ((p->name()) == "dire") {
+        Serial.print("Turn:");
+        Serial.println(p->value());
+      };
+
+      //                   Serial.print("Param value: ");
+      //                   Serial.println(p->value());
+    };                                                          //    end of for loop
+
+
+  });                                                              // server on END
+
+
+
+  // Route to load style.css file
+  server.on("/jquery-1.11.3.min.js", HTTP_GET, [](AsyncWebServerRequest * request)
   {
     request->send(SPIFFS, "/jquery-1.11.3.min.js", "text/javascript");
 
     Serial.println(" JS jquery GET ");
-      
+
   });
 
-                                                                                // Route to load style.css file
-  server.on("/roundslider.min.js", HTTP_GET, [](AsyncWebServerRequest *request)
+  // Route to load style.css file
+  server.on("/compass.png", HTTP_GET, [](AsyncWebServerRequest * request)
+  {
+    request->send(SPIFFS, "/compass.png", "image/png");
+
+    Serial.println(" PNG file GET ");
+
+  });
+
+  // Route to load style.css file
+  server.on("/roundslider.min.js", HTTP_GET, [](AsyncWebServerRequest * request)
   {
     request->send(SPIFFS, "/roundslider.min.js", "text/javascript");
 
     Serial.println(" min JS GET ");
-      
+
   });
 
-  
-                                                                                  // Route to load style.css file
-  server.on("/roundslider.min.css", HTTP_GET, [](AsyncWebServerRequest *request)
+
+  // Route to load style.css file
+  server.on("/roundslider.min.css", HTTP_GET, [](AsyncWebServerRequest * request)
   {
     request->send(SPIFFS, "/roundslider.min.css", "text/css");
-    
+
     Serial.println(" min css GET ");
-    
+
   });
 
 
-                                                                                //  Method GET - query from MEGA to to web page tag is / from_MEGA
-  server.on("/from_MEGA", HTTP_GET, [](AsyncWebServerRequest *request)
-    {
-      request->send_P(200, "text/plain", string_to_JS().c_str());               // return value to web page JS
-      
-    }); 
+  //  Method GET - query from MEGA to to web page tag is / from_MEGA
+  server.on("/from_MEGA", HTTP_GET, [](AsyncWebServerRequest * request)
+  {
+    request->send_P(200, "text/plain", string_to_JS().c_str());               // return value to web page JS
+
+  });
 
 
-                                                                                  // Start server
+  // Start server
   server.begin();
 
 }                                                                                 //  END OF setup
 
 
 
-                                                                                  // reservation 
+// reservation
 String processor(const String& var)
-{  }
+{
+  return var;
+}
 
 
 
 
 String string_to_JS()                                                             // funtion to be called with GET from_MEGA tag
-  {
-    return from_mega;
-  }
+{
+  return from_mega;
+}
 
 
- 
+
 void loop()
 {
-  if (Serial.available() > 0) 
-    {
-      serial_read();                                            // read string from Mega
-      from_mega=buf;
-       
-    }  
+  if (Serial.available() > 0)
+  {
+    serial_read();                                            // read string from Mega
+    from_mega = buf;
+
+  }
 }
 
 
@@ -176,32 +205,35 @@ void loop()
 
 void serial_read()                                    // read serial in string, save received characters to buff[]
 {
-  boolean done=0; 
+  boolean done = 0;
 
-  while (done==0)                                     // wait end of message LF
+  while (done == 0)                                   // wait end of message LF
+  {
+    while (Serial.available() > 0)
     {
-        while(Serial.available() > 0)
-        {
-          char char_in = Serial.read();
-        
-          if(char_in>13)                              // is input char above CR
-                {   
-                 buf[is]=char_in;
-                 is++;
-                }
-  
-          if(char_in==10){                            // check if end of message  LF   
-                    buf[is]=0;                        // end of string
-                    is=0;
-                    done=1;
-                  }
-    
-        }                                            // end of while serial available  
+      char char_in = Serial.read();
 
-        if(done==1) { done=0; break; }
+      if (char_in > 13)                           // is input char above CR
+      {
+        buf[is] = char_in;
+        is++;
+      }
+
+      if (char_in == 10) {                        // check if end of message  LF
+        buf[is] = 0;                      // end of string
+        is = 0;
+        done = 1;
+      }
+
+    }                                            // end of while serial available
+
+    if (done == 1) {
+      done = 0;
+      break;
+    }
 
   }                                                 // while END , End of message  LF
-   
-      // Serial.println(buf);
-      
+
+  // Serial.println(buf);
+
 }                                                   // end of serial read
