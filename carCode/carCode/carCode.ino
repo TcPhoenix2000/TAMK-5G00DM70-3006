7d43a30ca91e61ce926c6d992e8802b36b21c6c2
Tc_phoenix_ <tibo.coppens@outlook.com>
2024-03-06 12:40:26 +0200

    adding comments

--- carCode/carCode/carCode.ino
+++ carCode/carCode/carCode.ino
@@ -56,9 +56,9 @@ void setup() {
 
 void loop() {
   switch (program) {
-    case 0:
+    case 0:// Code for program 0 local drive
       Serial.println("program 0");  // week 1 & 2 exersize 2 part 1
-      // Code for program 0 local drive
+      
       remoteControl = false;
       checkRemote();
       while (analogRead(A9) >= 490 && analogRead(A9) <= 550 && analogRead(A8) >= 460 && analogRead(A8) <= 518) stay_put();
@@ -68,51 +68,62 @@ void loop() {
       while (analogRead(A8) < 460) SetSpeed(100), go_forward();
 
       break;
-    case 1:
+
+    case 1:// Code for program 1 remote control
       Serial.println("program 1");  // week 1 & 2 exersize 2 part 2
-      // Code for program 1 remote control
       remoteControl = true;
       checkRemote();
       stay_put();
       parseSerialData();
       break;
-    case 2:
+
+    case 2:// Step 2: Send the distance value via ESP with a command of your choice
       Serial.println("program 2");  // week 3 exersize 3 step 2
-      // Code for program 2 somthing else
+      
       remoteControl = true;
       checkRemote();
       stay_put();
       parseSerialData();
       break;
-    case 3:
+
+    case 3:// drive L of potentio value
       Serial.println("program 3");  // week 3 exersize 3 step 1
+      // Step 1: Create a program that drives your car to the distance set by 
+      // potentiometer (value x) and after reaching that value, rotates 90 degrees right 
+      // (using only one wheel) and then drives the same distance again
+      
       remoteControl = false;
       checkRemote();
-      // program 2 exe
+
+      // program 3 exe
       executeMovementSequence(joystickPotentio());
       program = 4;
       break;
-    case 4:
+
+    case 4:// follow program
       Serial.println("program 4");  // week 3 exersize 2 step 1
       // follow program
+      // Create a program that drives your car to a set distance from object/wall
       maintainDistance(20);
       break;
-    case 5:
+
+    case 5:// Instead of fixed 20 cm distance, the value is read from potentiometer
       Serial.println("program 5");  // week 3 exersize 2 step 2
-      // program 2 exe
+      // program 5 exe
+      // Instead of fixed 20 cm distance, the value is read from potentiometer
       stay_put();
       maintainDistance(joystickPotentio());
-      Serial.println(joystickPotentio());
       break;
-    case 6:
+
+    case 6:// calculate M² 
       Serial.println("program 6");  // week 4 exersize 1 step 1
-      // program 2 exe
+      // program 6 exe
       stay_put();
       executeSquareMeter();
       delay(2000);
       program = 7;
       break;
-    case 7:
+    case 7:// drive square
       Serial.println("program 7"); // week 5 exersize 2
       driveSquare();
       program = 8;
